3a11a8568218bc355c5441e94791d34137d09fc6
polyquail-finance <polyquail.finance@gmail.com>
2021-07-27 13:36:29 +0800

    change card number

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -29,7 +29,7 @@ export const lightColors: Colors = {
   textDisabled: "#666171",
   textSubtle: "#c9c4d4",
   borderColor: "#524B63",
-  card: "#27262c",
+  card:  "#152324",// "#27262c",
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
   },
